2588057adf8c4e47173fa20f53f5fddc7bfbfa87
Ezd13 <ozenhulya72@gmail.com>
2025-10-26 19:03:01 +0100

    modification sur comptes.sh selon l'exercice 4

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 # Script : comptes.sh
-# Objet : compter le nombre de FICHIERS .ann contenant "Location" pour chaque année
+# Objectif : compter le nombre de fichiers .ann contenant "Location" pour chaque année
 
 echo "Argument donné : $1"
 
 CHEMIN="$HOME/PPE1/Exercices/Exercice1/ann"
 
 if [[ ! -d $CHEMIN ]]; then
-    echo "Erreur, ce dossier $CHEMIN existe pas."
+    echo "Erreur, ce dossier $CHEMIN n'existe pas."
     exit 1
 fi
 
-echo "Nombre de lieux en 2016 :"
-cat $CHEMIN/2016/*.ann | grep "Location" | wc -l 
-
-echo "Nombre de lieux en 2017 :"
-cat $CHEMIN/2017/*.ann | grep "Location" | wc -l
-
-echo "Nombre de lieux en 2018 :"
-cat $CHEMIN/2018/*.ann | grep "Location" | wc -l
+# Exercice 4 : Boucle sur les années
+for ANNEE in 2016 2017 2018
+do
+    echo "Nombre de lieux en $ANNEE :"
+    cat "$CHEMIN/$ANNEE"/*.ann | grep "Location" | wc -l
+done
 
