7fb2be1610d1a2f2e8884df6f28b9d4d3c665ef1
Ezd13 <ozenhulya72@gmail.com>
2025-10-26 15:17:56 +0100

    Exercice 1: script comptes.sh compter les locations par année

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,13 +1,22 @@
+#!/usr/bin/env bash
+# Script : comptes.sh
+# Objet : compter le nombre de FICHIERS .ann contenant "Location" pour chaque année
 
-echo "argument donné: $1"
+echo "Argument donné : $1"
 
-CHEMIN=$1
+CHEMIN="$HOME/PPE1/Exercices/Exercice1/ann"
 
-echo "Nombre de lieux en 2016"
-cat cat 2016/*/*.ann | grep Location | wc -1 
+if [[ ! -d $CHEMIN ]]; then
+    echo "Erreur, ce dossier $CHEMIN existe pas."
+    exit 1
+fi
 
-echo "Nombre de lieux en 2017"
-cat cat 2017/*/*.ann | grep Location | wc -1 
+echo "Nombre de lieux en 2016 :"
+cat $CHEMIN/2016/*.ann | grep "Location" | wc -l 
 
-echo "Nombre de lieux en 2018"
-cat cat 2018/*/*.ann | grep Location | wc -1 
+echo "Nombre de lieux en 2017 :"
+cat $CHEMIN/2017/*.ann | grep "Location" | wc -l
+
+echo "Nombre de lieux en 2018 :"
+cat $CHEMIN/2018/*.ann | grep "Location" | wc -l
+
