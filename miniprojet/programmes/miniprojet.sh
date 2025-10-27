284b93b2b087e8f6c1e3b92e65f7cc4943b3f3ee
Ezd13 <ozenhulya72@gmail.com>
2025-10-27 16:25:57 +0100

    miniprojet exercice 2 /1

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -19,9 +19,20 @@ if [ ! -r "$fichier" ]; then
 fi
 
 # Question 3 : Afficher le numéro de ligne avant chaque URL (séparé par une tabulation)
+#exercice 2 modifications pour le code HTTP 
 n=1
 while read -r line; do
-    echo -e "${n}\t${line}"
+    url="$line"
+    # si le schéma http/https manque, on ajoute https://
+    if [[ ! "$url" =~ ^https?:// ]]; then
+        url="https://$url"
+    fi
+
+    # récupération du code HTTP avec curl
+    code=$(curl -s -L -o /dev/null -w "%{http_code}" "$url")
+
+    # affichage : numéro<TAB>URL<TAB>code
+    printf "%s\t%s\t%s\n" "$n" "$line" "$code"
     ((n++))
 done < "$fichier"
 
